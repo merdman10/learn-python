04ad3925a1b3f167b557e07f27e1db401c82d650
Monica Erdman <monicaerdman10@gmail.com>
2018-04-10 22:05:49 -0700

    help me

--- basic/list2.py
+++ basic/list2.py
@@ -13,17 +13,20 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-  # +++your code here+++
-  return
-
+  short = nums 
+  for i in range(len(nums)-1):
+    if nums[i] != nums[i+1]:
+     short.append(nums[i]) 
+  return short
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-  # +++your code here+++
-  return
+  merge = list1 + list2
+  sort = sorted(merge)
+  return sort
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
